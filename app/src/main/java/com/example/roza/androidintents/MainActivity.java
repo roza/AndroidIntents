8ba62184b77e0c30e46724d0b294868e04288477
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2018-02-14 14:47:13 +0100

    added button to launch contact inten

--- app/src/main/java/com/example/roza/androidintents/MainActivity.java
+++ app/src/main/java/com/example/roza/androidintents/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.roza.androidintents;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText edt;
     static final int LAUNCH_OTHER_ACTIVITY = 1;
+    static final int REQUEST_SELECT_CONTACT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
                dialPhoneNumer("0123456789");
             }
         });
+
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Launch Contact Intent", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                selectContact();
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +72,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
+    private void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+
     public void startOtherActivity(){
         Intent intent = new Intent(this,OtherActivity.class);
         // Paramètres à passer à OtherActivity
@@ -75,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MainActivity","Launching Other Activity");
         startActivityForResult(intent, LAUNCH_OTHER_ACTIVITY);
     }
+    /* Information de retour de OtherActivity */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         // Check which request we're responding to
@@ -83,6 +106,21 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 String message = intent.getStringExtra("text");
                 edt.setText(message);
+            }
+        }
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            Uri contactUri = intent.getData();
+            String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(contactUri, projection,
+                    null, null, null);
+            // If the cursor returned is valid, get the phone number
+            if (cursor != null && cursor.moveToFirst()) {
+                int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(numberIndex);
+                if (number != null)
+                    edt.setText(number);
+                else
+                    edt.setText("Tel Inconnu");
             }
         }
     }
